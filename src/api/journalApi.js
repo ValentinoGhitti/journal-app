81d6a6d5cc669a181accc12452b73754606113c3
Vachu <89685214+ValentinoGhitti@users.noreply.github.com>
2023-06-26 03:24:54 -0300

    env update

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const journalApi = axios.create({
-  baseURL: 'https://journal-app-e6c9e-default-rtdb.firebaseio.com'
+  baseURL: 'API_KEY'
 })
 
 export default journalApi
